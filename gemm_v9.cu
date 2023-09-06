1a096b1e2e7f400c7e9fcfdcb4b4caa19e163362
root <zhangys2@weitu.com>
2023-09-06 01:43:21 -0700

    Put the last loop out of the big loop

--- gemm_v9.cu
+++ gemm_v9.cu
@@ -139,7 +139,6 @@ __global__ void gemm(
         }
     }
 
-    int limitK=BLOCK_SIZE_K;
 
     A=ADDR(A,pitch_A,by*BLOCK_SIZE_M,0);
     B=ADDR(B,pitch_B,0,bx*BLOCK_SIZE_N);  
@@ -183,37 +182,36 @@ __global__ void gemm(
     }
 
     int write_stage_idx = 1;
-    for(int tile_idx=BLOCK_SIZE_K;tile_idx<K+BLOCK_SIZE_K;tile_idx += BLOCK_SIZE_K)
-    {
+    int load_stage_idx;
+
+    for(int tile_idx=BLOCK_SIZE_K;tile_idx<K;tile_idx += BLOCK_SIZE_K){
         // load next tile from global mem
-        if(tile_idx< K){
-            #pragma unroll
-            for ( int i = 0 ; i < BLOCK_SIZE_M ; i += A_TILE_ROW_STRIDE) {
-                int ldg_index = i / A_TILE_ROW_STRIDE * 4;
-                if(A_TILE_ROW_START + i<M)
-                    FETCH_FLOAT4(ldg_a_reg[ldg_index]) = FETCH_FLOAT4(*ADDR(
-                        A,
-                        pitch_A,
-                        A_TILE_ROW_START + i,
-                        A_TILE_COL + tile_idx));
-                    } 
-            #pragma unroll
-            for ( int i = 0 ; i < BLOCK_SIZE_K; i += B_TILE_ROW_STRIDE) {
-                int ldg_index = i / B_TILE_ROW_STRIDE * 4;
-                if(tile_idx + B_TILE_ROW_START + i<K)
-                    FETCH_FLOAT4(ldg_b_reg[ldg_index]) = FETCH_FLOAT4(*ADDR(
-                        B,
-                        pitch_B,
-                        tile_idx + B_TILE_ROW_START + i,
-                        B_TILE_COL));
-                }               
-        }
-
-        int load_stage_idx = write_stage_idx ^ 1;
-
-        limitK=tile_idx>K?K%BLOCK_SIZE_K:limitK;
         #pragma unroll
-        for(int j=0; j<limitK-1; ++j){
+        for ( int i = 0 ; i < BLOCK_SIZE_M ; i += A_TILE_ROW_STRIDE) {
+            int ldg_index = i / A_TILE_ROW_STRIDE * 4;
+            if(A_TILE_ROW_START + i<M)
+                FETCH_FLOAT4(ldg_a_reg[ldg_index]) = FETCH_FLOAT4(*ADDR(
+                    A,
+                    pitch_A,
+                    A_TILE_ROW_START + i,
+                    A_TILE_COL + tile_idx));
+                } 
+        #pragma unroll
+        for ( int i = 0 ; i < BLOCK_SIZE_K; i += B_TILE_ROW_STRIDE) {
+            int ldg_index = i / B_TILE_ROW_STRIDE * 4;
+            if(tile_idx + B_TILE_ROW_START + i<K)
+                FETCH_FLOAT4(ldg_b_reg[ldg_index]) = FETCH_FLOAT4(*ADDR(
+                    B,
+                    pitch_B,
+                    tile_idx + B_TILE_ROW_START + i,
+                    B_TILE_COL));
+            }               
+
+        load_stage_idx = write_stage_idx ^ 1;
+
+        
+        #pragma unroll
+        for(int j=0; j<BLOCK_SIZE_K-1; ++j){
             // load next tile from shared mem to register 
             // load A from shared memory to register
             #pragma unroll
@@ -235,47 +233,83 @@ __global__ void gemm(
             }
         }
 
-        if(tile_idx < K){
-            #pragma unroll
-            for ( int i = 0 ; i < BLOCK_SIZE_M ; i += A_TILE_ROW_STRIDE) {
-                int ldg_index = i / A_TILE_ROW_STRIDE * 4;
-                As[write_stage_idx][A_TILE_COL][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index];
-                As[write_stage_idx][A_TILE_COL+1][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+1];
-                As[write_stage_idx][A_TILE_COL+2][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+2];
-                As[write_stage_idx][A_TILE_COL+3][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+3];
-            }
-            // load B from global memory to shared memory
-            #pragma unroll
-            for ( int i = 0 ; i < BLOCK_SIZE_K; i += B_TILE_ROW_STRIDE) {
-                int ldg_index = i / B_TILE_ROW_STRIDE * 4;
-                FETCH_FLOAT4(Bs[write_stage_idx][B_TILE_ROW_START + i][B_TILE_COL]) = FETCH_FLOAT4(ldg_b_reg[ldg_index]);
-            }
-            // use double buffer, only need one sync
-            __syncthreads();
-            // switch
-            write_stage_idx ^= 1;
+        #pragma unroll
+        for ( int i = 0 ; i < BLOCK_SIZE_M ; i += A_TILE_ROW_STRIDE) {
+            int ldg_index = i / A_TILE_ROW_STRIDE * 4;
+            As[write_stage_idx][A_TILE_COL][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index];
+            As[write_stage_idx][A_TILE_COL+1][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+1];
+            As[write_stage_idx][A_TILE_COL+2][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+2];
+            As[write_stage_idx][A_TILE_COL+3][A_TILE_ROW_START + i]=ldg_a_reg[ldg_index+3];
         }
+        // load B from global memory to shared memory
+        #pragma unroll
+        for ( int i = 0 ; i < BLOCK_SIZE_K; i += B_TILE_ROW_STRIDE) {
+            int ldg_index = i / B_TILE_ROW_STRIDE * 4;
+            FETCH_FLOAT4(Bs[write_stage_idx][B_TILE_ROW_START + i][B_TILE_COL]) = FETCH_FLOAT4(ldg_b_reg[ldg_index]);
+        }
+        // use double buffer, only need one sync
+        __syncthreads();
+        // switch
+        write_stage_idx ^= 1;
 
         // load first tile from shared mem to register of next iter
         // load A from shared memory to register
         #pragma unroll
         for (int thread_y = 0; thread_y < THREAD_SIZE_Y; thread_y += 4) {
-            FETCH_FLOAT4(frag_a[limitK%2][thread_y]) = FETCH_FLOAT4(As[load_stage_idx^1][0][THREAD_SIZE_Y * ty + thread_y]);
+            FETCH_FLOAT4(frag_a[BLOCK_SIZE_K%2][thread_y]) = FETCH_FLOAT4(As[load_stage_idx^1][0][THREAD_SIZE_Y * ty + thread_y]);
         }
         // load B from shared memory to register
         #pragma unroll
         for (int thread_x = 0; thread_x < THREAD_SIZE_X; thread_x += 4) {
-            FETCH_FLOAT4(frag_b[limitK%2][thread_x]) = FETCH_FLOAT4(Bs[load_stage_idx^1][0][THREAD_SIZE_X * tx + thread_x]);
+            FETCH_FLOAT4(frag_b[BLOCK_SIZE_K%2][thread_x]) = FETCH_FLOAT4(Bs[load_stage_idx^1][0][THREAD_SIZE_X * tx + thread_x]);
         }
         //compute last tile mma THREAD_SIZE_X x THREAD_SIZE_Y
         #pragma unroll
         for (int thread_y = 0; thread_y < THREAD_SIZE_Y_limit; ++thread_y) {
             #pragma unroll
             for (int thread_x = 0; thread_x < THREAD_SIZE_X_limit; ++thread_x) {
-                accum[thread_y][thread_x] += frag_a[(limitK+1)%2][thread_y] * frag_b[(limitK+1)%2][thread_x];
+                accum[thread_y][thread_x] += frag_a[(BLOCK_SIZE_K+1)%2][thread_y] * frag_b[(BLOCK_SIZE_K+1)%2][thread_x];
+            }
+        }    
+    }
+
+
+    load_stage_idx = write_stage_idx ^ 1;
+
+    int limitK=K%BLOCK_SIZE_K?K%BLOCK_SIZE_K:BLOCK_SIZE_K;
+
+    #pragma unroll
+    for(int j=0; j<limitK-1; ++j){
+    // load next tile from shared mem to register 
+    // load A from shared memory to register
+        #pragma unroll
+        for (int thread_y = 0; thread_y < THREAD_SIZE_Y; thread_y += 4) {
+            FETCH_FLOAT4(frag_a[(j+1)%2][thread_y]) = FETCH_FLOAT4(As[load_stage_idx][j+1][THREAD_SIZE_Y * ty + thread_y]);
+        }
+        // load B from shared memory to register
+        #pragma unroll
+        for (int thread_x = 0; thread_x < THREAD_SIZE_X; thread_x += 4) {
+            FETCH_FLOAT4(frag_b[(j+1)%2][thread_x]) = FETCH_FLOAT4(Bs[load_stage_idx][j+1][THREAD_SIZE_X * tx + thread_x]);
+        }
+        // compute C THREAD_SIZE_X x THREAD_SIZE_Y
+        #pragma unroll
+        for (int thread_y = 0; thread_y < THREAD_SIZE_Y_limit; ++thread_y) {
+            #pragma unroll
+            for (int thread_x = 0; thread_x < THREAD_SIZE_X_limit; ++thread_x) {
+                accum[thread_y][thread_x] += frag_a[j%2][thread_y] * frag_b[j%2][thread_x];
             }
         }
-       
+    }
+    // switch
+    write_stage_idx ^= 1;
+
+    //compute last tile mma THREAD_SIZE_X x THREAD_SIZE_Y
+    #pragma unroll
+    for (int thread_y = 0; thread_y < THREAD_SIZE_Y_limit; ++thread_y) {
+        #pragma unroll
+        for (int thread_x = 0; thread_x < THREAD_SIZE_X_limit; ++thread_x) {
+            accum[thread_y][thread_x] += frag_a[(limitK+1)%2][thread_y] * frag_b[(limitK+1)%2][thread_x];
+        }
     }
 
     // store back to C
